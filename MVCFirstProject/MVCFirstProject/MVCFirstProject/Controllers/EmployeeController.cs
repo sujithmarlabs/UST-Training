e84660f27e21100afe7b2c62ffe0d62b3cfc8d14
Sujith S <sujith.marlabs@gmail.com>
2022-07-19 11:48:33 +0530

    My First MSbuild Project added

---
+++ MVCFirstProject/MVCFirstProject/MVCFirstProject/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFirstProject.Models;
+
+namespace MVCFirstProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        { 
+            Employee employee = new Employee()
+                {
+                EmployeeId = 1001,
+                EmployeeName = "Sujith S",
+                EmployeeLocation = "Kochi"
+                };
+            return View(employee);
+        }
+    }
+}
